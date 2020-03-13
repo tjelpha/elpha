84825d81f9d9123c4ab3ad80b9a64ef205697cc4
tjelpha <tian8anti@gmail.com>
2020-03-13 08:49:57 +0700

    upload projek vue

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderElphatis />
+    <HeroElphatis />
+    <WomenSection />
+    <InstaElphatis />
+    <PartnerLogo />
+    <FooterElphatis />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import HeaderElphatis from "@/components/HeaderElphatis.vue";
+import HeroElphatis from "@/components/HeroElphatis.vue";
+import WomenSection from "@/components/WomenSection.vue";
+import InstaElphatis from "@/components/InstaElphatis.vue";
+import PartnerLogo from "@/components/PartnerLogo.vue";
+import FooterElphatis from "@/components/FooterElphatis.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderElphatis,
+    HeroElphatis,
+    WomenSection,
+    InstaElphatis,
+    PartnerLogo,
+    FooterElphatis
+  }
+};
+</script>
